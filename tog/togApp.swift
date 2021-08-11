338838b860ebdea7897b0ae14cde1b63a2b5eafa
stjomd <at.zhukov@gmail.com>
2021-08-11 20:42:54 +0200

    Initial Commit

---
+++ tog/togApp.swift
@@ -0,0 +1,17 @@
+//
+//  togApp.swift
+//  tog
+//
+//  Created by Artem Zhukov on 11.08.21.
+//
+
+import SwiftUI
+
+@main
+struct togApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
