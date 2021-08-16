574eea7ec273b44447922584d04f915de80854a6
stjomd <at.zhukov@gmail.com>
2021-08-16 17:19:09 +0200

    Add home view UI tests

--- tog/togApp.swift
+++ tog/togApp.swift
@@ -12,6 +12,8 @@ import CoreData.NSManagedObjectContext
 @main
 struct togApp: App {
   
+  private let isUITest = ProcessInfo.processInfo.arguments.contains("UITEST")
+  
   /// Core Data's managed object context type.
   typealias CDContext = NSManagedObjectContext
   /// Swinject Container.
@@ -29,7 +31,15 @@ struct togApp: App {
   
   private func registerDependencies() {
     Self.container.register(CDContext.self)    { _ in CoreDataStore.shared.persistentContainer.viewContext }
-    Self.container.register(DataService.self)  { r in OEBBDataService(context: r.resolve(CDContext.self)!) }
+    if isUITest {
+      // Mock data (ÖBB's open data, predownloaded)
+      Self.container.register(DataService.self)  { r in OEBBDataService(context: r.resolve(CDContext.self)!) }
+    } else {
+      // TODO: Register a real data service
+      // Currently also mock data, since ÖBB doesn't offer any open API. In a real app the following line
+      // should register a data service that actually fetches data from an API.
+      Self.container.register(DataService.self)  { r in OEBBDataService(context: r.resolve(CDContext.self)!) }
+    }
     Self.container.register(ColorService.self) { _ in ColorService() }
   }
   
