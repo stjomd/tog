3bf88044fd92c341cb3df2759e0ffebbb14558f2
stjomd <at.zhukov@gmail.com>
2021-08-19 00:20:34 +0200

    Add documentation

--- server/railway/src/main/java/com/stjomd/railway/generator/util/CSVHandler.java
+++ server/railway/src/main/java/com/stjomd/railway/generator/util/CSVHandler.java
@@ -11,8 +11,18 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Map;
 
+/**
+ * Provides methods for working with CSV files.
+ */
 public interface CSVHandler {
 
+    /**
+     * Iterates over rows in a CSV file and performs an action on each iteration.
+     *
+     * @param fileName the name of the file (with the extension) to be read.
+     * @param operator a function that accepts cells (an array of String) and is called on each iteration.
+     * @return the amount of rows iterated over.
+     */
     static int forEachRowIn(String fileName, CSVRowOperator operator) {
         BufferedReader reader = openOEBBFile(fileName);
         ICsvMapReader csvReader = new CsvMapReader(reader, CsvPreference.STANDARD_PREFERENCE);
@@ -23,12 +33,18 @@ public interface CSVHandler {
                 operator.operator(line);
             }
             csvReader.close();
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new CSVException("Error while iterating over file", e);
         }
         return csvReader.getRowNumber() - 1;
     }
 
+    /**
+     * Opens a file and returns a buffered reader for it.
+     *
+     * @param fileName the name of the file in the Resources folder under <code>static/oebb-data/</code> to be opened.
+     * @return the BufferedReader of the file.
+     */
     private static BufferedReader openOEBBFile(String fileName) {
         URL url = CSVHandler.class.getResource("/static/oebb-data/" + fileName);
         if (url == null)
