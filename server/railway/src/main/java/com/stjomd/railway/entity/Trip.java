d4aa8b862b5126ae3b524116c245c20a321016c0
stjomd <at.zhukov@gmail.com>
2021-08-22 16:08:48 +0200

    Generate routes

--- server/railway/src/main/java/com/stjomd/railway/entity/Trip.java
+++ server/railway/src/main/java/com/stjomd/railway/entity/Trip.java
@@ -6,9 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.Set;
 
@@ -32,10 +35,15 @@ public class Trip {
     @OneToMany(mappedBy = "trip")
     private Set<Halt> halts;
 
-    public Trip(Long id, String headsign, String shortName) {
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "route_id", referencedColumnName = "id", nullable = false)
+    private Route route;
+
+    public Trip(Long id, String headsign, String shortName, Route route) {
         this.id = id;
         this.headsign = headsign;
         this.shortName = shortName;
+        this.route = route;
     }
 
 }
--- server/railway/src/main/java/com/stjomd/railway/generator/TripGenerator.java
+++ server/railway/src/main/java/com/stjomd/railway/generator/TripGenerator.java
@@ -1,43 +1,56 @@
 package com.stjomd.railway.generator;
 
+import com.stjomd.railway.entity.Route;
 import com.stjomd.railway.entity.Trip;
 import com.stjomd.railway.generator.util.CSVHandler;
+import com.stjomd.railway.repository.RouteRepository;
 import com.stjomd.railway.repository.TripRepository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
+@Slf4j
 @Profile("datagen")
+@DependsOn({"routeGenerator"})
 @Component
 public class TripGenerator {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TripGenerator.class);
     private final TripRepository tripRepository;
+    private final RouteRepository routeRepository;
 
     @Autowired
-    public TripGenerator(TripRepository tripRepository) {
+    public TripGenerator(TripRepository tripRepository, RouteRepository routeRepository) {
         this.tripRepository = tripRepository;
+        this.routeRepository = routeRepository;
     }
 
     @PostConstruct
     private void generate() {
         if (tripRepository.findAll().size() > 0) {
-            LOGGER.info("Trips have already been generated");
+            log.info("Trips have already been generated");
         } else {
-            LOGGER.info("Generating trips...");
+            log.info("Generating trips...");
+            List<Route> routes = routeRepository.findAll();
             int count = CSVHandler.forEachRowIn("trips.txt", cells -> {
                 Long   id        = Long.valueOf(cells.get("trip_id"));
+                Long   routeId   = Long.valueOf(cells.get("route_id"));
                 String headsign  = cells.get("trip_headsign");
                 String shortName = cells.get("trip_short_name");
-                Trip trip = new Trip(id, headsign, shortName);
+                // Entities
+                Route route = routes.stream().filter(r -> r.getId().equals(routeId)).findFirst()
+                        .orElse(Route.builder().id(routeId).build());
+                Trip trip = new Trip(id, headsign, shortName, route);
                 tripRepository.save(trip);
             });
-            LOGGER.info("Generated {} trip(s)", count);
+            log.info("Generated {} trip(s)", count);
         }
     }
 
